9d78989d4b05e69915a66a07770cab2f188319d8
Doug Ellery <104483244+doug-ellery@users.noreply.github.com>
2026-08-04 21:15:04 -0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cpp
@@ -0,0 +1,27 @@
+class MinStack {
+private:
+    vector<int> my_stack;
+    vector<int> mins;
+public:
+    MinStack() {
+
+    }
+    
+    void push(int val) {
+        my_stack.push_back(val);
+        mins.push_back(mins.empty() || val < mins.back() ? val : mins.back());
+    }
+    
+    void pop() {
+        my_stack.pop_back();
+        mins.pop_back();
+    }
+    
+    int top() {
+        return my_stack.back();
+    }
+    
+    int getMin() {
+        return mins.back();
+    }
+};
